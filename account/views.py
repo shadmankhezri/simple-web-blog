af0eb5d122d376381321531b70bfec86fe3be244
shadman <khezri.shadman@gmail.com>
2023-09-03 20:53:59 +0330

    add account page for log in

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render , redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login , logout
 
 # Create your views here.
 def user_login(request):
     if request.user.is_authenticated == True:
         return redirect('/')
+
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -13,3 +15,30 @@ def user_login(request):
             login(request , user)
             return redirect('/')
     return render(request , "account/login.html" , {})
+
+
+
+def user_register(request):
+    context = {"errors":[]}
+    if request.user.is_authenticated == True:
+        return redirect('/')
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2= request.POST.get('password2')
+        if password1 != password2:
+            context['errors'].append('password are not same')
+            return render(request, "account/register.html", context)
+
+        user = User.objects.create(username=username , password=password1 , email=email)
+        login(request , user)
+        return redirect('/')
+    return render(request , "account/register.html" , {})
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
