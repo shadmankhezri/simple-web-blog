febd67e7c8188cbfee44ff482b37b8f772c20322
shadman <khezri.shadman@gmail.com>
2023-09-07 01:20:37 +0330

    add search

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = "blog"
 urlpatterns = [
     path('detail/<slug:slug>' , views.post_detail , name="article_detail"),
     path('list' , views.article_list , name="articles_list"),
-    path('category/<int:pk>' , views.category_detail , name="category_detail")
+    path('category/<int:pk>' , views.category_detail , name="category_detail"),
+    path('search/' , views.search , name="search_articles"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,9 @@ from django.core.paginator import Paginator
 def post_detail(request , slug):
     article = get_object_or_404(Article, slug=slug)
     if request.method == 'POST':
+        parent_id = request.POST.get('parent_id')
         body = request.POST.get('body')
-        Comment.objects.create(body=body , article=article , user=request.user)
+        Comment.objects.create(body=body , article=article , user=request.user , parent_id=parent_id)
 
     return render(request, "blog/article_detail.html", {"article":article})
 
@@ -25,3 +26,13 @@ def category_detail(request , pk=None):
     category = get_object_or_404(Category , id=pk)
     articles = category.article_set.all()
     return render(request , "blog/articles_list.html" , {'articles':articles})
+
+
+
+def search(request):
+    q = request.GET.get('q')
+    articles = Article.objects.filter(title__icontains=q)
+    page_number = request.GET.get('page')
+    paginator = Paginator(articles , 2)
+    objects_list = paginator.get_page(page_number)
+    return render(request , "blog/articles_list.html" , {"articles":objects_list})
